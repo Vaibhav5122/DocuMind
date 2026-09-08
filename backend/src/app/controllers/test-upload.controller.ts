dc1c440cc43d609908c6f2e8c61d113b641b26e5
Vaibhav5122 <vwaghmode5757@gmail.com>
2026-09-08 22:26:14 +0530

    feat: make testUploadController asynchronous and enhance Cloudinary upload handling

--- backend/src/app/controllers/test-upload.controller.ts
+++ backend/src/app/controllers/test-upload.controller.ts
@@ -2,20 +2,21 @@ import { ApiError } from "../utils/ApiError.js";
 import type { Request, Response } from "express";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadDocumentToCloudinary } from "../services/cloudinary.service.js";
-import { upload } from "../middlewares/upload.middleware.js";
 
 export class UploadController {
-  public testUploadController(req: Request, res: Response) {
+  public async testUploadController(req: Request, res: Response) {
     if (!req.file) {
       throw ApiError.badRequest("File is required");
     }
-    console.log("Donee");
+    // console.log("Donee", req.user?.id);
+    // console.log(mongoose.isValidObjectId(req.user?.id));
     //Cloudinary upload
-    uploadDocumentToCloudinary({
+    const result = await uploadDocumentToCloudinary({
       buffer: req.file.buffer,
       originalFileName: req.file.originalname,
     });
 
+    console.log("Resulttt", result);
     return ApiResponse.ok(res, "File uploaded", {
       file: {
         originalName: req.file.originalname,
