6bcd9f17a81467f6487af8a624e5abd8eb767dbf
Vaibhav5122 <vwaghmode5757@gmail.com>
2026-09-08 21:02:02 +0530

    feat: implement Cloudinary upload in testUploadController method

--- backend/src/app/controllers/test-upload.controller.ts
+++ backend/src/app/controllers/test-upload.controller.ts
@@ -1,6 +1,8 @@
 import { ApiError } from "../utils/ApiError.js";
 import type { Request, Response } from "express";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { uploadDocumentToCloudinary } from "../services/cloudinary.service.js";
+import { upload } from "../middlewares/upload.middleware.js";
 
 export class UploadController {
   public testUploadController(req: Request, res: Response) {
@@ -8,6 +10,12 @@ export class UploadController {
       throw ApiError.badRequest("File is required");
     }
     console.log("Donee");
+    //Cloudinary upload
+    uploadDocumentToCloudinary({
+      buffer: req.file.buffer,
+      originalFileName: req.file.originalname,
+    });
+
     return ApiResponse.ok(res, "File uploaded", {
       file: {
         originalName: req.file.originalname,
